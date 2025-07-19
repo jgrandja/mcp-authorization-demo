c3e230ee58e6fcd581d248f4c52fe578bee869b7
Joe Grandja <10884212+jgrandja@users.noreply.github.com>
2025-07-19 08:16:54 -0400

    Integrate MCP Client and Server

--- mcp-client/src/main/java/sample/config/OAuth2ClientCustomizations.java
+++ mcp-client/src/main/java/sample/config/OAuth2ClientCustomizations.java
@@ -21,14 +21,20 @@ import java.util.function.Consumer;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesMapper;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizationFailureHandler;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.endpoint.RestClientAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.RestClientClientCredentialsTokenResponseClient;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
@@ -41,8 +47,6 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * @author Joe Grandja
@@ -50,6 +54,9 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties({OAuth2ClientProperties.class})
 public class OAuth2ClientCustomizations {
+
+    @Value("${spring.ai.mcp.client.sse.connections.server1.url}")
+    private String targetResource;
 
     @Bean
     public ManagedClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties clientProperties) {
@@ -66,13 +73,8 @@ public class OAuth2ClientCustomizations {
                         OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI);
         authorizationRequestResolver.setAuthorizationRequestCustomizer((builder) ->
             builder.additionalParameters((params) -> {
-                if (RequestContextHolder.getRequestAttributes() instanceof ServletRequestAttributes servletRequestAttributes) {
-                    String resource = servletRequestAttributes.getRequest().getParameter("resource");
-                    if (StringUtils.hasText(resource)) {
-                        // Pass the resource parameter in authorization requests
-                        params.put("resource", resource);
-                    }
-                }
+                // Pass the resource parameter in authorization requests
+                params.put("resource", this.targetResource);
             })
         );
         return authorizationRequestResolver;
@@ -95,6 +97,20 @@ public class OAuth2ClientCustomizations {
         return accessTokenResponseClient;
     }
 
+    @Bean("oauth2-rest-client")
+    public RestClient oauth2RestClient(
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            OAuth2AuthorizedClientManager authorizedClientManager) {
+        OAuth2ClientHttpRequestInterceptor requestInterceptor =
+                new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
+        OAuth2AuthorizationFailureHandler authorizationFailureHandler =
+                OAuth2ClientHttpRequestInterceptor.authorizationFailureHandler(authorizedClientRepository);
+        requestInterceptor.setAuthorizationFailureHandler(authorizationFailureHandler);
+        return RestClient.builder()
+                .requestInterceptor(requestInterceptor)
+                .build();
+    }
+
     @Bean
     public Consumer<DefaultOAuth2AuthorizedClientManager> authorizedClientManagerCustomizer(
             final OAuth2AuthorizedClientRepository authorizedClientRepository) {
@@ -114,18 +130,40 @@ public class OAuth2ClientCustomizations {
             });
     }
 
-    @Bean("oauth2-rest-client")
-    public RestClient oauth2RestClient(
-            OAuth2AuthorizedClientRepository authorizedClientRepository,
-            OAuth2AuthorizedClientManager authorizedClientManager) {
-        OAuth2ClientHttpRequestInterceptor requestInterceptor =
-                new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
-        OAuth2AuthorizationFailureHandler authorizationFailureHandler =
-                OAuth2ClientHttpRequestInterceptor.authorizationFailureHandler(authorizedClientRepository);
-        requestInterceptor.setAuthorizationFailureHandler(authorizationFailureHandler);
-        return RestClient.builder()
-                .requestInterceptor(requestInterceptor)
+    static AuthorizedClientServiceOAuth2AuthorizedClientManager serviceBasedAuthorizedClientManager(
+            ManagedClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientService authorizedClientService,
+            final String targetResource) {
+
+        RestClientClientCredentialsTokenResponseClient accessTokenResponseClient =
+                new RestClientClientCredentialsTokenResponseClient();
+        accessTokenResponseClient.addParametersConverter(grantRequest -> {
+            MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+            // Pass the resource parameter in access token requests
+            parameters.set("resource", targetResource);
+            return parameters;
+        });
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials(clientCredentials ->
+                        clientCredentials.accessTokenResponseClient(accessTokenResponseClient))
                 .build();
+
+        AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager =
+                new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientService);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        authorizedClientManager.setAuthorizationSuccessHandler((authorizedClient, principal, attributes) -> {
+            if (authorizedClient.getAccessToken().getScopes().contains("client.create")) {
+                // Dynamic client registration access tokens are scoped to 'client.create' and are one-time-use only,
+                // so no need to save the OAuth2AuthorizedClient since the access token is no longer valid.
+                return;
+            }
+            authorizedClientService.saveAuthorizedClient(authorizedClient, principal);
+        });
+
+        return authorizedClientManager;
     }
 
 }
